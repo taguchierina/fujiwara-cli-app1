ae6d613835cdf4f7cd08253fb1cbd0ddb6a80e1c
Erina Taguchi <kd1257044@st.kobedenshi.ac.jp>
2018-11-29 17:25:53 +0900

    11/30提出

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-// 他のjsファイル(index_github.jsなど)を参考にして、
-// JavaScriptコードをここに書く
+
+const endpoint = "https://connpass.com/api/v1/event/"
 
 // 1. ボタンがクリックされたときのイベントを追加
 const button = document.getElementById("main");  // HTML中のボタン（id="main"）を検索
 button.addEventListener("click", function () {
-  // 2. APIにリクエストを送り、レスポンスを受け取る
-  get();
+  // ユーザIDをテキストフィールド（id="userId"）から取得
+  const keyword = document.getElementById("keyword_or").value;
+
+  // ユーザ情報をGitHub APIから取得
+  keywordSearch(keyword)
 });
 
 // 1. ここまで
 
-// 2. APIにリクエストを送り、レスポンスを受け取る
-function get() {
+// 2. GitHub APIにリクエストを送り、レスポンスを受け取る
+function keywordSearch(keyword) {
   // XMLHttpRequest(XHR)オブジェクトの初期化
   const request = new XMLHttpRequest();
 
   // URLを組み立てて、リクエストを準備する（まだリクエストは未送信）
-
-  const idEncoded = encodeURIComponent("978-4-0486-6452-3");  // URL用にエンコード
-  const url = `https://api.openbd.jp/v1/get?isbn=${idEncoded}&pretty`;  // テンプレートリテラルを使用（バッククォートで囲む）
+  const keywordEncoded = encodeURIComponent(keyword);  // URL用にエンコード
+  // const url = `https://connpass.com/api/v1/event/?keyword=${keywordEncoded}` ;  // テンプレートリテラルを使用（バッククォートで囲む）
+  const url = `https://connpass.com/api/v1/event/?keyword=${keywordEncoded}`
   request.open("GET", url);
 
   // サーバからレスポンスが返ってきた場合の処理を登録
   request.addEventListener("load", function (event) {
+    console.log(event)
     // エラーハンドリング：①HTTPステータスコードが「200 OK」以外の場合
     if (event.target.status !== 200) {
       console.log("Error: HTTPステータスが「200 OK」ではありません");
       console.log(`${event.target.status}: ${event.target.statusText}`);
       return;
     }
-
+    // alert(event)
     // 正常時の処理：3. レスポンス（JSONテキスト）を加工し、DOMに追加する
-    show(event);
+    showUserInfo(event);
   });
 
   // エラーハンドリング：②サーバとの通信に際してエラーが発生した場合
@@ -43,8 +47,11 @@ function get() {
   request.send();
 }
 
+// 2. ここまで
+
 // 3. レスポンス（JSONテキスト）を加工し、DOMに追加する
-function show(event) {
+function showUserInfo(event) {
+
   // コンソールへの出力
   console.log(`HTTPステータスコード: ${event.target.status}`);
   console.log("返ってきたJSONテキスト:");
@@ -52,15 +59,35 @@ function show(event) {
 
   // レスポンス（JSONテキスト）を、JSON.parseメソッドで連想配列（オブジェクト）に変換する
   const text_json = event.target.responseText;  // レスポンステキスト（JSON）
-  const response = JSON.parse(text_json);       // JSONテキストをJavaScriptのオブジェクトに変換
+  const userInfo = JSON.parse(text_json);       // JSONテキストをJavaScriptのオブジェクトに変換
 
-  // コンソールにオブジェクトをデバッグ用出力する
-  console.dir(response);
+  //forぶんでかく
+  // HTMLテキスト（DOM）を組み立てる
+  // テンプレートリテラルを使用して、変数を埋め込む（注意：バッククォートを使う！）
+  console.log(userInfo.events)
+  let viewLog = ""
+  for (let index = 0; index < userInfo.events.length; index++) {
+    const element = userInfo.events[index];
+    console.log(element.title)
 
-  // responseを処理する
-  // 連想配列（オブジェクト）のキーを取り出す
-  console.log(response[0].summary.title);
+    const view = `
+          <h4>${element.title} (${element.event_id})</h4>
+          <dl>
+              <dt>場所</dt>
+              <dd>${element.address}</dd>
+              <dt>定員</dt>
+              <dd>${element.limit}</dd>
+              <dt>開催日時</dt>
+              <dd>${element.started_at}</dd>
+          </dl>
+          `;
+    viewLog = viewLog + view
+  }
+  // 追加先の<div id="result">を検索
+  const result = document.getElementById("result");
 
+  // HTMLテキスト（DOM）を<div id="result">の下に追加
+  result.innerHTML = viewLog;
 }
 
 // 3. ここまで
--- index_github.js
+++ index_github.js
@@ -2,22 +2,22 @@
 const button = document.getElementById("main");  // HTML中のボタン（id="main"）を検索
 button.addEventListener("click", function () {
   // ユーザIDをテキストフィールド（id="userId"）から取得
-  const nameId = document.getElementById("nameId").value;
+  const userId = document.getElementById("userId").value;
 
   // ユーザ情報をGitHub APIから取得
-  getUserInfo(nameId);
+  getUserInfo(userId);
 });
 
 // 1. ここまで
 
 // 2. GitHub APIにリクエストを送り、レスポンスを受け取る
-function getUserInfo(nameId) {
+function getUserInfo(userId) {
   // XMLHttpRequest(XHR)オブジェクトの初期化
   const request = new XMLHttpRequest();
 
   // URLを組み立てて、リクエストを準備する（まだリクエストは未送信）
-  const userIdEncoded = encodeURIComponent(nameId);  // URL用にエンコード
-  const url = `https://connpass.com/api/v1/event/`;  // テンプレートリテラルを使用（バッククォートで囲む）、URLをGithubからconnpassに変更
+  const userIdEncoded = encodeURIComponent(userId);  // URL用にエンコード
+  const url = `https://api.github.com/users/${userIdEncoded}`;  // テンプレートリテラルを使用（バッククォートで囲む）
   request.open("GET", url);
 
   // サーバからレスポンスが返ってきた場合の処理を登録
